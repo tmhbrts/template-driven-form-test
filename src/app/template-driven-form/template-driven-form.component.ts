0b8f4a5df22f33d7321d19695df1460b04fd92f0
Tim Huiberts <t.huiberts@lyanthe.com>
2024-12-09 10:28:37 +0100

    Include initial setup for template driven form

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+
+enum Equipment {
+  Desk = 'Desk',
+  Chair = 'Chair',
+  Computer = 'Computer',
+  Monitor = 'Monitor',
+  Keyboard = 'Keyboard',
+  Mouse = 'Mouse',
+  Printer = 'Printer',
+  Phone = 'Phone',
+}
+
+const departments = ['Development', 'Marketing', 'Sales', 'Support'];
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './template-driven-form.component.html',
-  styleUrl: './template-driven-form.component.scss'
+  styleUrl: './template-driven-form.component.scss',
 })
 export class TemplateDrivenFormComponent {
+  departments = departments;
+  equipmentOptions = Object.values(Equipment);
 
+  addTeamMember() {
+    console.log('Adding team member');
+  }
+
+  submitForm() {
+    console.log('Submitting form');
+  }
 }
