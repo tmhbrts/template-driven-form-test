3aaa7d98936b9a4ee5dbeb64502ceeacf8ac071a
Tim Huiberts <t.huiberts@lyanthe.com>
2024-12-09 16:06:48 +0100

    Include (async) conditional logic in reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,6 +9,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { map, shareReplay, startWith, Subject, tap } from 'rxjs';
+import { switchMap, timer } from 'rxjs';
 
 enum Equipment {
   Desk = 'Desk',
@@ -25,12 +29,14 @@ const departments = ['Development', 'Marketing', 'Sales', 'Support'];
 @Component({
   selector: 'app-reactive-form',
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
     MatListModule,
+    MatSlideToggleModule,
     MatButtonModule,
     MatIconModule,
   ],
@@ -40,6 +46,15 @@ const departments = ['Development', 'Marketing', 'Sales', 'Support'];
 })
 export class ReactiveFormComponent {
   private fb = inject(FormBuilder);
+  private defaultEquipementOptions: Equipment[] = [
+    Equipment.Desk,
+    Equipment.Chair,
+    Equipment.Computer,
+    Equipment.Monitor,
+    Equipment.Keyboard,
+    Equipment.Mouse,
+  ];
+
   equipmentOptions = Object.values(Equipment);
   departments = departments;
 
@@ -51,6 +66,7 @@ export class ReactiveFormComponent {
     ),
     contractPeriodStart: this.fb.control<Date | null>(null),
     contractPeriodEnd: this.fb.control<Date | null>(null),
+    defaultEquipmentOnly: this.fb.control<boolean>(false),
     requiredEquipment: this.fb.control<Equipment[]>([]),
     members: this.fb.array([
       this.fb.group({
@@ -62,6 +78,33 @@ export class ReactiveFormComponent {
       }),
     ]),
   });
+
+  teamNameControl = this.teamForm.controls.teamName;
+  departmentControl = this.teamForm.controls.department;
+  contractPeriodStartControl = this.teamForm.controls.contractPeriodStart;
+  contractPeriodEndControl = this.teamForm.controls.contractPeriodEnd;
+  defaultEquipementOnlyControl = this.teamForm.controls.defaultEquipmentOnly;
+  requiredEquipmentControl = this.teamForm.controls.requiredEquipment;
+  membersControl = this.teamForm.controls.members;
+
+  defaultEquipmentOnly$ = this.defaultEquipementOnlyControl.valueChanges.pipe(
+    startWith(false),
+    tap((value) => {
+      if (value) {
+        this.requiredEquipmentControl.setValue(this.defaultEquipementOptions);
+      }
+    }),
+  );
+
+  teamMemberSyncRequest$ = new Subject<void>();
+
+  //simulates a 3s response time
+  teamMemberSyncStatus$ = this.teamMemberSyncRequest$.pipe(
+    switchMap(() =>
+      timer(0, 1000).pipe(map((tick) => (tick < 3 ? true : false))),
+    ),
+    shareReplay(1),
+  );
 
   addTeamMember() {
     this.membersControl.push(
@@ -80,11 +123,4 @@ export class ReactiveFormComponent {
       console.log(this.teamForm.value);
     }
   }
-
-  teamNameControl = this.teamForm.controls.teamName;
-  departmentControl = this.teamForm.controls.department;
-  contractPeriodStartControl = this.teamForm.controls.contractPeriodStart;
-  contractPeriodEndControl = this.teamForm.controls.contractPeriodEnd;
-  requiredEquipmentControl = this.teamForm.controls.requiredEquipment;
-  membersControl = this.teamForm.controls.members;
 }
